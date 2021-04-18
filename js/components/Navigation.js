096ff9088c83f494b552781aba8834e9318b11e0
Kacper Nalepa <kacper.nlp@wp.pl>
2021-04-18 22:05:15 +0200

    last commit

--- js/components/Navigation.js
+++ js/components/Navigation.js
@@ -3,12 +3,14 @@ const CLASSES_FOR_BAR_AND_NAV = {
   barsClass: "bars--active",
   navClass: "nav--active",
 };
+const NAV_ELEMENTS_SELECTOR = ".nav__link";
 const NAV_ID = "nav";
 
 class Navigation {
   constructor() {
     this.bars = document.getElementById(BARS_ID);
     this.navigation = document.getElementById(NAV_ID);
+    this.navElements = document.querySelectorAll(NAV_ELEMENTS_SELECTOR);
   }
 
   init() {
@@ -16,6 +18,8 @@ class Navigation {
     if (innerWidth < 1024)
       this.bars.addEventListener("click", this.handleBarsClick);
     else window.addEventListener("scroll", this.handleScrollEvent);
+
+    this.handleEachNavLink();
   }
 
   handleBarsClick = () => {
@@ -31,6 +35,17 @@ class Navigation {
 
     if (scrollY < 100) this.navigation.classList.remove(navClass);
     else this.navigation.classList.add(navClass);
+  };
+
+  handleEachNavLink = () => {
+    const { barsClass, navClass } = CLASSES_FOR_BAR_AND_NAV;
+
+    this.navElements.forEach((element) =>
+      element.addEventListener("click", () => {
+        this.navigation.classList.remove(navClass);
+        this.bars.classList.remove(barsClass);
+      })
+    );
   };
 }
 
